b6f7cb185b6a19a6d5f854a221348b9f0f1a9d65
studilm <5081266+studilm@users.noreply.github.com>
2019-03-19 04:57:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('*', (req, res) => {
-    res.write('<h1><marquee direction=right>Hello from Express path `/` on Now 2.0!</marquee></h1>')
-    res.write('<h2>Go to <a href="/about">/about</a></h2>')
-    res.end()
+    res.sendFile(__dirname + '/index.html')
 })
 
 module.exports = app
